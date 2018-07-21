cd447350675dc92bf39febdf1b6ee8a821aa38e4
ChampBond <41200274+ChampBond@users.noreply.github.com>
2018-07-21 13:50:04 +0700

    Nambah P dan Commit
    Test

---
+++ src/modules/P.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./P.js";
+
+// class Button extends React.Component {
+//   render() {
+//     return <button> {this.props.children} </button>
+//   }
+// }
+
+function P(props) {
+  return <p style={{ paddlingTop: 10, fontSize: 20 }}>Test </p>;
+}
+export default P;
